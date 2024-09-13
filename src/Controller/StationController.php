d1896095a532f4a8973c7d8d1d288bf214c43c56
thierrysio <thierry28220@icloud.com>
2024-09-13 15:02:55 +0200

    ok

--- src/Controller/StationController.php
+++ src/Controller/StationController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Borne;
 use App\Entity\Station;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -27,11 +28,34 @@ class StationController extends AbstractController
         $entityManager->persist($station);
         $entityManager->flush();
 
-        return new Response('la station dont le nom est : '. $station->getLibelleEmplacement() .' a été créée avec succès');
+        return new Response('la station dont le nom est : '. $station->getLibelleEmplacement() .' a été créée avecsucces');
+
+    }
+
+    #[Route('/station/creeravecborne', name: 'app_station_creeravecborne')]
+    public function creeravecborne(EntityManagerInterface $entityManager): Response
+    {
+        $borne = new Borne();
+        $borne->setDateDerniereRevision(new \DateTime());
+        $borne->setIndiceCompteurUnites(100);
+
+        $borne2 = new Borne();
+        $borne2->setDateDerniereRevision(new \DateTime());
+        $borne2->setIndiceCompteurUnites(50);
+        
+        $entityManager->persist($borne);
+        $entityManager->persist($borne2);
+
+        $station = new Station();
+        $station->setLibelleEmplacement('station 01');
+        $station->addLesBorne($borne);
+        $station->addLesBorne($borne2);
 
 
+        $entityManager->persist($station);
+        $entityManager->flush();
 
-
+        return new Response('la station dont le nom est : '. $station->getLibelleEmplacement() .' a été créée avecsucces');
 
     }
 }
