95ccdcbf7c5011b79d5db8bd8a49cd3b5120132b
thierrysio <thierry28220@icloud.com>
2024-09-13 13:50:40 +0200

    ok

--- src/Controller/StationController.php
+++ src/Controller/StationController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\Station;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -14,5 +16,22 @@ class StationController extends AbstractController
         return $this->render('station/index.html.twig', [
             'controller_name' => 'StationController',
         ]);
+    }
+
+    #[Route('/station/creer', name: 'app_station_creer')]
+    public function creer(EntityManagerInterface $entityManager): Response
+    {
+        $station = new Station();
+        $station->setLibelleEmplacement('station 01');
+
+        $entityManager->persist($station);
+        $entityManager->flush();
+
+        return new Response('la station dont le nom est : '. $station->getLibelleEmplacement() .' a été créée avec succès');
+
+
+
+
+
     }
 }
